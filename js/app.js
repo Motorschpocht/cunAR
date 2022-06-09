47436b04f6dfdc4ed69f10900145892a6eb4e7e5
Daniel Schramm <daniel.schramm@emvi.com>
2022-06-09 16:44:16 +0200

    Fixed updating marker.

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@ document.addEventListener("alpine:init", () => {
     Alpine.data("app", () => ({
         page: "start",
         startUpStep: 1,
-        planeText: "",
         expand: true,
 
         resizePlane(height, width) {
-            var element = document.getElementById("htmlElement");
+            var element = document.getElementById("htmlElement" + startUp[this.startUpStep].plane);
+            console.log("Resize");
             element.style.minHeight = 1000*height + "px";
             element.style.minWidth = 1000*width + "px";
         },
@@ -25,23 +25,19 @@ document.addEventListener("alpine:init", () => {
             this.page = id;
         },
 
-        updatePlaneText(text) {
-            this.planeText = text;
-        },
-
         updateStep(step) {
             this.startUpStep = step;
+            console.log("Update");
 
             if(startUp[this.startUpStep].mapTop && startUp[this.startUpStep].mapLeft) {
                 this.updateMapPin(startUp[this.startUpStep].mapTop, startUp[this.startUpStep].mapLeft);
             }
 
-            if(startUp.ar) {
+            if(startUp[this.startUpStep].ar) {
+                console.log("Is AR");
                 this.resizePlane(startUp[this.startUpStep].height, startUp[this.startUpStep].width);
                 this.movePlane("plane"+startUp[this.startUpStep].plane, startUp[this.startUpStep].xpos, startUp[this.startUpStep].ypos, startUp[this.startUpStep].zpos);
                 this.rotatePlane("plane"+startUp[this.startUpStep].plane, startUp[this.startUpStep].xrot, startUp[this.startUpStep].yrot, startUp[this.startUpStep].zrot);
-            } else {
-                // TODO...
             }
         },
 
